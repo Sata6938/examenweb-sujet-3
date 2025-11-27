85a7eb665f676b0df4062adc0dd49e0308f91fbd
lk140 <max.hemp@icloud.com>
2025-11-27 15:05:06 +0000

    ENFINNNNNNNNNNNNNNNNNNNNN

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("bonjours a toute et a tous")
+
+
+const titre = document.querySelector('#bouton27')
+if (titre) {
+  titre.addEventListener('click', () => {
+    open("niveau1.html");
+  });
+}
+
+const titre2 = document.querySelector('#bouton28')
+if (titre) {
+  titre.addEventListener('click', () => {
+    open("niveau2.html");
+  });
+}
+
+
+
